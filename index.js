6c1b4c7d3f3be6d81bd68cda64885b95e36953f9
Tan <nttan.env@gmail.com>
2020-06-27 23:18:59 +0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('index', { title: '1-hello', message: 'TODO - LIST' })
+  res.render('index', { title: '1-hello', message: 'TODO - LIST', 
+  	todoList: [
+  		'Go to market',
+  		'Cooking rice',
+  		'Washing dishes',
+  		'Learing at coderX-Tokyo'
+  	]
+   })
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
