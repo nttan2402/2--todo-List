73ba7fd0cea61ca1adc760b1b68d4b63acdd03c8
Tan <nttan.env@gmail.com>
2020-06-29 14:49:15 +0700

    update

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.set('views', './views');
-app.set('view engine', 'pug');
-
-app.get('/', function (req, res) {
-  res.render('index', { title: '1-hello', message: 'TODO - LIST', 
-  	todoList: [
+var todos = [
   		'Go to market',
-  		'Cooking rice',
+  		'nấu cơm',
   		'Washing dishes',
   		'Learing at coderX-Tokyo'
   	]
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/todos', function (req, res) {
+  res.render('index', { title: '1-hello', message: 'TODO - LIST', 
+  	todoList: todos
    })
-})
+});
+
+app.get('/todos', function (req, res) {
+ 	var q = req.query.q;
+ 	var matchedActive = todos.filter(function(active){
+ 		return  active.indexOf(q) !== -1;
+ 	});
+ 	res.render('index', {title: '3-Query Parameters', message: 'TODO - LIST',
+ 		search: q,
+ 		todoList: matchedActive
+ 	});
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
